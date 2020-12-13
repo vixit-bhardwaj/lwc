d117f0fc3b410a4dba6fa07d28377fe65d0e1df2
vixit-bhardwaj <vixit.bhardwaj@icloud.com>
2020-12-13 15:26:08 -0800

    First Commit

---
+++ lwc-recipes-project/force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
@@ -0,0 +1,15 @@
+/**
+ * @description       :
+ * @author            : VIXIT BHARDWAJ
+ * @group             :
+ * @last modified on  : 09-12-2020
+ * @last modified by  : VIXIT BHARDWAJ
+ * Modifications Log
+ * Ver   Date         Author           Modification
+ * 1.0   09-12-2020   VIXIT BHARDWAJ   Initial Version
+ **/
+import { LightningElement } from "lwc";
+
+export default class ConditionalRendering extends LightningElement {
+  isDetailAvailable = true;
+}
