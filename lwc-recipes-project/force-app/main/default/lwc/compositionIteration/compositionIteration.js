21fc6b5cedd842761808cf297a3c0564c9a7b766
vixit-bhardwaj <vixit.bhardwaj@icloud.com>
2020-12-30 10:55:42 -0800

    All Commit

--- lwc-recipes-project/force-app/main/default/lwc/compositionIteration/compositionIteration.js
+++ lwc-recipes-project/force-app/main/default/lwc/compositionIteration/compositionIteration.js
@@ -1,3 +1,13 @@
+/**
+ * @description       : 
+ * @author            : Vixit Bhardwaj - TA AtoS
+ * @group             : 
+ * @last modified on  : 12-27-2020
+ * @last modified by  : Vixit Bhardwaj - TA AtoS
+ * Modifications Log 
+ * Ver   Date         Author                     Modification
+ * 1.0   12-27-2020   Vixit Bhardwaj - TA AtoS   Initial Version
+**/
 import { LightningElement } from 'lwc';
 
 export default class CompositionIteration extends LightningElement {
@@ -7,6 +17,7 @@ export default class CompositionIteration extends LightningElement {
             Name: 'Amy Taylor',
             Title: 'VP of Engineering',
             Phone: '6172559632',
+            Sex:'F',
             Picture__c:
                 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/amy_taylor.jpg'
         },
@@ -15,6 +26,7 @@ export default class CompositionIteration extends LightningElement {
             Name: 'Michael Jones',
             Title: 'VP of Sales',
             Phone: '6172551122',
+            Sex:'M',
             Picture__c:
                 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/michael_jones.jpg'
         },
@@ -23,6 +35,7 @@ export default class CompositionIteration extends LightningElement {
             Name: 'Jennifer Wu',
             Title: 'CEO',
             Phone: '6172558877',
+            Sex:'F',
             Picture__c:
                 'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/jennifer_wu.jpg'
         }
--- lwc-recipes-project/force-app/main/default/lwc/contactTile/contactTile.js
+++ lwc-recipes-project/force-app/main/default/lwc/contactTile/contactTile.js
@@ -1,5 +1,15 @@
+/**
+ * @description       : 
+ * @author            : Vixit Bhardwaj - TA AtoS
+ * @group             : 
+ * @last modified on  : 12-27-2020
+ * @last modified by  : Vixit Bhardwaj - TA AtoS
+ * Modifications Log 
+ * Ver   Date         Author                     Modification
+ * 1.0   12-27-2020   Vixit Bhardwaj - TA AtoS   Initial Version
+**/
 import { LightningElement, api } from 'lwc';
 
 export default class ContactTile extends LightningElement {
-    @api contact;
+    @api contact;  
 }
